12ba14ed05f567b25ee8d96b3f051baa9d55e08f
vpha0025 <115525238+vpha0025@users.noreply.github.com>
2022-10-17 04:51:08 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,19 @@
+var vg_1 = "code/js/map.vg.json";
+vegaEmbed('#map', vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "code/js/bar1.vg.json";
+vegaEmbed('#bar1', vg_2).then(function(result) {
+}).catch(console.error);
+
+var vg_3 = "code/js/bar2.vg.json";
+vegaEmbed('#bar2', vg_3).then(function(result) {
+}).catch(console.error);
+
+var vg_4 = "code/js/pie.vg.json";
+vegaEmbed('#pie', vg_4).then(function(result) {
+}).catch(console.error);
+
+var vg_5 = "code/js/line.vg.json";
+vegaEmbed('#line', vg_5).then(function(result) {
+}).catch(console.error);
